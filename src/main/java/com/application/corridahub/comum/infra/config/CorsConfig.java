a89bfea4233b3e654784df85b15d1799212f83c7
Gabriel Lima <gabriel.pdcase@emap.emap.ma.gov.br>
2023-11-14 14:37:00 -0300

    fix cors yml

--- src/main/java/com/application/corridahub/comum/infra/config/CorsConfig.java
+++ src/main/java/com/application/corridahub/comum/infra/config/CorsConfig.java
@@ -1,20 +1,16 @@
 package com.application.corridahub.comum.infra.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer{
-	
-	@Value("${app.cors-origin}")
-	private String corsOrigin;  
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins(corsOrigin)
+			.allowedOrigins("*")
 			.allowedMethods("*")
 			.allowedHeaders("*");
 	}
